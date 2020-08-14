abd8f2c769d685355df493d9edb96dbaa1ebdb1d
Julien MOIRENC <julien.moirenc@gmail.com>
2020-08-14 11:18:59 +0200

    change problem and solution games field to 1024 long in the database

--- sql/mysql-db.sql
+++ sql/mysql-db.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `problem` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `solution` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `problem` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `solution` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `start` datetime COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `end` datetime COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `solvingtime` int(11) DEFAULT NULL,
